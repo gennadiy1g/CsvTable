b4908a182fff4938241d815b02d49448259aca04
Gennadiy <gennadiy1g@gmx.com>
2019-11-18 16:33:21 -0500

    WIP

--- CsvTable.cpp
+++ CsvTable.cpp
@@ -143,6 +143,7 @@ std::wstring FileLines::getLine(std::size_t lineNum)
                 BOOST_LOG_SEV(gLogger, bltrivial::trace) << "line=" << boost::trim_right_copy(blocale::conv::utf_to_utf<wchar_t>(line))
                                                          << ", tellg()=" << mFileStream.tellg() << FUNCTION_FILE_LINE;
             }
+        } else {
         }
     }
     return boost::trim_right_copy(blocale::conv::utf_to_utf<wchar_t>(line));
