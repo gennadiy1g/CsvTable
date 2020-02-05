72a57f60a91ce141a0110e5697e6d3fa4d3528e0
Gennadiy <gennadiy1g@gmx.com>
2020-02-05 11:36:32 -0500

    Trying to identify and fix a bug.

--- CsvTable.cpp
+++ CsvTable.cpp
@@ -198,8 +198,8 @@ std::wstring FileLines::getLine(std::size_t lineNum)
                 }
             } else {
                 auto pos = mPosBetweenSamples.at(mPosBetweenSamples.size() - 1);
+                mFileStream.seekg(pos);
                 for (std::size_t i = 0; i < rem - mPosBetweenSamples.size(); ++i) {
-                    mFileStream.seekg(pos);
                     std::getline(mFileStream, line);
                     BOOST_LOG_SEV(gLogger, bltrivial::trace) << "line=[" << boost::trim_right_copy(blocale::conv::utf_to_utf<wchar_t>(line))
                                                              << "], tellg()=" << mFileStream.tellg() << FUNCTION_FILE_LINE;
