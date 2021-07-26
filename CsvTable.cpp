6e117d2711fd95c62026b9e6846f92c4a14ba666
Gennadiy <gennadiy1g@gmx.com>
2021-07-26 16:50:38 -0400

    Small change to logging.

--- CsvTable.cpp
+++ CsvTable.cpp
@@ -239,8 +239,8 @@ std::wstring FileLines::getLine(std::size_t lineNum)
                     << ", mFileStream.tellg()=" << mFileStream.tellg();
                 if(morePosBetweenSamples()) {
                     mPosBetweenSamples.push_back(mFileStream.tellg());
-                    BOOST_LOG_SEV(gLogger, bltriv::trace) << "mPosBetweenSamples[" << mPosBetweenSamples.size() - 1
-                                                          << "]=" << mPosBetweenSamples.back() << FUNCTION_FILE_LINE;
+                    BOOST_LOG_SEV(gLogger, bltriv::trace)
+                        << "mPosBetweenSamples[" << mPosBetweenSamples.size() - 1 << "]=" << mPosBetweenSamples.back();
                 }
             }
         } else {
