0f96b483c11df45723ec44d1fd038d10af126b20
Gennadiy <gennadiy1g@gmx.com>
2021-09-17 16:13:11 -0400

    Change type of mNumLinesBetweenSamples to atomic_size_t.

--- CsvTable.hpp
+++ CsvTable.hpp
@@ -63,7 +63,7 @@ private:
     std::atomic_size_t mNumLines { 0 };                  // Number of lines in the file
     std::size_t mApproxNumLines { 0 };                   // Approximate number of lines in the file
     std::vector<bfs::ifstream::pos_type> mPosSampleLine; // Positions of sample lines
-    std::size_t mNumLinesBetweenSamples { 1 };           // Number of lines between successive sample lines
+    std::atomic_size_t mNumLinesBetweenSamples { 1 };    // Number of lines between successive sample lines
     bool mIsNumLinesLimitReached { false };
 
     decltype(mPosSampleLine) mPosBetweenSamples; // Positions of lines between sample lines
