17296e92b4ada03fa368cda80cb8b697d5f42e0e
Gennadiy <gennadiy1g@gmx.com>
2022-03-22 18:13:01 -0400

    Add kBackslash constant.

--- SepChars.hpp
+++ SepChars.hpp
@@ -1,4 +1,4 @@
 #pragma once
 
 constexpr wchar_t kTab{L'\t'}, kPipe{L'|'}, kSemicolon{L';'}, kComma{L','}, kSpace{L' '}, kDoubleQuote{L'\"'},
-    kSingleQuote{L'\''}, kNull{L'\0'};
+    kSingleQuote{L'\''}, kBackslash{L'\\'}, kNull{L'\0'};
